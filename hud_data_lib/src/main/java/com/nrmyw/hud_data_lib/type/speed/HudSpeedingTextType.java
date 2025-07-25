88f58a9a44c53dde6215c0b8703e31db11ebfecd
lixiao <lixiao@nb-666.com>
2025-07-25 15:45:53 +0800

    修改speeding

--- hud_data_lib/src/main/java/com/nrmyw/hud_data_lib/type/speed/HudSpeedingTextType.java
+++ hud_data_lib/src/main/java/com/nrmyw/hud_data_lib/type/speed/HudSpeedingTextType.java
@@ -1,6 +1,6 @@
 package com.nrmyw.hud_data_lib.type.speed;
 
-public enum HudSpeedingShowType {
+public enum HudSpeedingTextType {
     //直接发送指令
     WRITE((byte)0x00),//正常白色
     RED((byte)0x01),//警告红色
@@ -8,7 +8,7 @@ public enum HudSpeedingShowType {
     YELLOW((byte)0x03),//黄色
     ;
     byte type;
-    HudSpeedingShowType(byte type){
+    HudSpeedingTextType(byte type){
         this.type=type;
     }
 
