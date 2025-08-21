3d919e4e0ff8f1ee2adeac654b86ca89b89bc5be
lixiao <lixiao@nb-666.com>
2025-08-21 16:00:31 +0800

    修改到达信息的封装

--- hud_data_lib/src/main/java/com/nrmyw/hud_data_lib/type/reach/HudReachType.java
+++ hud_data_lib/src/main/java/com/nrmyw/hud_data_lib/type/reach/HudReachType.java
@@ -1,9 +1,21 @@
 package com.nrmyw.hud_data_lib.type.reach;
 
 public enum HudReachType {
-    NONE,//默认样式
-    AM,//早上的样式
-    PM,//下午的样式
-    ARRIVAL,//到达时间的样式
+//    NONE,
+//    AM,
+//    PM,
 
+    NONE((byte)0x00),//默认样式
+    AM((byte)0x01),//早上的样式
+    PM((byte)0x02),//下午的样式
+    ARRIVAL((byte)0x03),//到达时间的样式
+    ;
+    byte type;
+    HudReachType(byte type){
+        this.type=type;
+    }
+
+    public byte getType() {
+        return type;
+    }
 }
