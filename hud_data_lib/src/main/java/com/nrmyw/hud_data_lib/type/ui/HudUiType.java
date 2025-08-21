534b29b92608b3669cee85b0e619ee6c2f7e4d11
lixiao <lixiao@nb-666.com>
2025-08-21 17:12:25 +0800

    UI加参数

--- hud_data_lib/src/main/java/com/nrmyw/hud_data_lib/type/ui/HudUiType.java
+++ hud_data_lib/src/main/java/com/nrmyw/hud_data_lib/type/ui/HudUiType.java
@@ -1,9 +1,20 @@
 package com.nrmyw.hud_data_lib.type.ui;
 
 public enum HudUiType {
-    UI_1,
-    UI_2,
-    UI_3,
-    UI_4,
-    UI_5,
+    UI_1((byte) 0x00),
+    UI_2((byte) 0x00),
+    UI_3((byte) 0x00),
+    UI_4((byte) 0x00),
+    UI_5((byte) 0x00),
+    ;
+    byte type;
+
+    HudUiType(byte type){
+
+        this.type=type;
+    }
+
+    public byte getType() {
+        return type;
+    }
 }
