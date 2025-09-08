a8f3042fb8dee2d1585f70be60726daccb9c3558
lixiao <lixiao@nb-666.com>
2025-09-08 13:12:13 +0800

    fskdjflks

--- hud_data_lib/src/main/java/com/nrmyw/hud_data_lib/type/time/HudTimeType.java
+++ hud_data_lib/src/main/java/com/nrmyw/hud_data_lib/type/time/HudTimeType.java
@@ -1,16 +1,15 @@
-//package com.nrmyw.hud_data_lib.type.time;
-//
-//public enum HudTimeType {
-//    NONE((byte)0x00),
-//    AM((byte)0x01),
-//    PM((byte)0x02),
-//    ;
-//    byte type;
-//    HudTimeType(byte type){
-//        this.type=type;
-//    }
-//
-//    public byte getType() {
-//        return type;
-//    }
-//}
+package com.nrmyw.hud_data_lib.type.time;
+
+public enum HudTimeType {
+    NONE(),
+    CHINA(),
+    KOREA(),
+
+    ;
+
+    HudTimeType(){
+
+    }
+
+
+}
