874bc02c79963d82a7721b7e9ce759a39b5f3569
Li Xiao <lixiao@nb-666.com>
2026-02-25 17:35:39 +0800

    新增亮度值

--- hud_data_lib/src/main/java/com/nrmyw/hud_data_lib/type/HudCmdType.java
+++ hud_data_lib/src/main/java/com/nrmyw/hud_data_lib/type/HudCmdType.java
@@ -59,7 +59,7 @@ public enum HudCmdType {
     NEW_LANE_INFORMATION((byte)0x86),//车道信息显示
     NEW_TURN_TYPE((byte)0x87),//转向信息显示
     NOTIFICATION((byte)0x88),//信息提醒
-
+    LUMINANCE_PERCENT((byte)0x89),//信息提醒
 
 
     OTA((byte)0xA5)
