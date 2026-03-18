2e8ae0b3b28e65015f7164edc67bc3e187134200
Li Xiao <lixiao@nb-666.com>
2026-03-18 19:29:10 +0800

    代码忘记加hud

--- hud_data_lib/src/main/java/com/nrmyw/hud_data_lib/type/notification/HudNotificationIconType.java
+++ hud_data_lib/src/main/java/com/nrmyw/hud_data_lib/type/notification/HudNotificationIconType.java
@@ -1,12 +1,12 @@
 package com.nrmyw.hud_data_lib.type.notification;
 
-public enum NotificationIconType {
+public enum HudNotificationIconType {
     HIDE((byte)0x00),
     EXIT((byte)0x01),
     ;
 
     byte type;
-    NotificationIconType(byte type){
+    HudNotificationIconType(byte type){
         this.type=type;
     }
 
